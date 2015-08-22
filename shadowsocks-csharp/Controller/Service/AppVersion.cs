d6bae9d6c5fa39777211f98dfd452b1dc0c9e007
chiypg <chiypg@users.noreply.github.com>
2015-08-22 22:45:11 +0800

    add missing file

---
+++ shadowsocks-csharp/Controller/Service/AppVersion.cs
@@ -0,0 +1,18 @@
+﻿using Shadowsocks.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using SimpleJson;
+
+namespace Shadowsocks.Controller
+{
+    public class AppVersion
+    {
+        public const string Version = "2.5.7";
+    }
+
+}
